3bf22c78a21cdd23d06086d7a8c86cc6c5dd11db
Dax Harris <matteovh@gmail.com>
2023-12-05 17:25:02 -0500

    wip item information

---
+++ lia/src/modals/ItemEditModal/ItemEditModal.tsx
@@ -0,0 +1,318 @@
+import { useForm } from "@mantine/form";
+import { AccessReference } from "../../types/extra";
+import { GroceryList, ListItem, RecipeList } from "../../types/list";
+import {
+    ActionIcon,
+    Button,
+    Center,
+    Group,
+    Image,
+    Modal,
+    NumberInput,
+    Paper,
+    Pill,
+    Rating,
+    Select,
+    SimpleGrid,
+    Stack,
+    TagsInput,
+    Text,
+    TextInput,
+    rem,
+} from "@mantine/core";
+import {
+    IconBuildingStore,
+    IconCategoryFilled,
+    IconCurrencyDollar,
+    IconEdit,
+    IconHash,
+    IconInfoCircle,
+    IconLinkPlus,
+    IconMapPin,
+    IconMeat,
+    IconShoppingBag,
+    IconStarHalfFilled,
+    IconTrashFilled,
+} from "@tabler/icons-react";
+import { useTranslation } from "react-i18next";
+import "./modal.scss";
+import { capitalize } from "lodash";
+import { useMediaQuery } from "@mantine/hooks";
+import { useApiMethods } from "../../api";
+import { useCallback } from "react";
+
+export function ItemEditModal({
+    item,
+    access,
+    list,
+    open,
+    setOpen,
+}: {
+    item: ListItem;
+    access: AccessReference;
+    list: GroceryList | RecipeList;
+    open: boolean;
+    setOpen: (open: boolean) => void;
+}) {
+    const { t } = useTranslation();
+    const isDesktop = useMediaQuery("(min-width: 48em)", true);
+    const form = useForm<
+        Omit<
+            ListItem,
+            "id" | "list_id" | "alternative" | "checked" | "recipe" | "added_by"
+        >
+    >({
+        initialValues: {
+            name: item.name,
+            quantity: item.quantity,
+            categories: item.categories,
+            price: item.price,
+            location: item.location,
+            linked_item: item.linked_item,
+        },
+        validate: {
+            name: (values) =>
+                values.length > 0
+                    ? null
+                    : t("modals.editItem.fields.name.error"),
+        },
+    });
+
+    const api = useApiMethods();
+
+    const removeLinkedItem = useCallback(() => {
+        if (api) {
+            api.list.updateItem(access.type, access.reference, item.id, {
+                linked_item: null,
+            });
+        }
+    }, [item, api]);
+
+    return (
+        <Modal
+            title={
+                <Group gap="sm" justify="space-between" style={{ flexGrow: 2 }}>
+                    <IconInfoCircle />
+                    <Text>{t("modals.editItem.title")}</Text>
+                </Group>
+            }
+            opened={open}
+            onClose={() => setOpen(false)}
+            size="xl"
+        >
+            <Stack gap="sm">
+                <TextInput
+                    leftSection={<IconMeat size={16} />}
+                    label={t("modals.editItem.fields.name.label")}
+                    {...form.getInputProps("name")}
+                />
+                <SimpleGrid
+                    cols={{ base: 1, md: 3 }}
+                    spacing="sm"
+                    verticalSpacing="sm"
+                >
+                    <NumberInput
+                        hideControls
+                        leftSection={<IconHash size={16} />}
+                        rightSection={
+                            <Select
+                                data={[
+                                    "kg",
+                                    "lb",
+                                    "liters",
+                                    "gallons",
+                                    "quarts",
+                                ]}
+                                clearable
+                                {...form.getInputProps("quantity.unit")}
+                                style={{
+                                    input: {
+                                        fontWeight: 500,
+                                        borderTopLeftRadius: "0 !important",
+                                        borderBottomLeftRadius: "0 !important",
+                                        width: rem(92),
+                                        marginRight: rem(-2),
+                                    },
+                                }}
+                                rightSectionWidth={28}
+                                placeholder={t(
+                                    "modals.editItem.fields.quantity.unitPlaceholder"
+                                )}
+                                className="quantity-input-unit-select"
+                            />
+                        }
+                        label={t("modals.editItem.fields.quantity.label")}
+                        rightSectionWidth={92}
+                        {...form.getInputProps("quantity.amount")}
+                    />
+
+                    <TextInput
+                        label={t("modals.editItem.fields.location.label")}
+                        leftSection={<IconMapPin size={16} />}
+                        value={form.values.location ?? ""}
+                        onChange={(event) =>
+                            form.setFieldValue(
+                                "location",
+                                event.target.value.length > 0
+                                    ? event.target.value
+                                    : null
+                            )
+                        }
+                    />
+                    <NumberInput
+                        leftSection={<IconCurrencyDollar size={16} />}
+                        label={t("modals.editItem.fields.price.label")}
+                        min={0}
+                        decimalScale={2}
+                        {...form.getInputProps("price")}
+                    />
+                </SimpleGrid>
+                <TagsInput
+                    label={t("modals.editItem.fields.categories.label")}
+                    leftSection={<IconCategoryFilled size={16} />}
+                    {...form.getInputProps("categories")}
+                />
+                <Paper
+                    p="sm"
+                    radius="sm"
+                    shadow="sm"
+                    className="linked-item-card"
+                >
+                    {item.linked_item ? (
+                        <Group
+                            className="linked-item-display"
+                            gap="sm"
+                            align="start"
+                            wrap="nowrap"
+                        >
+                            {item.linked_item.images[0] ? (
+                                <Image
+                                    src={item.linked_item.images[0]}
+                                    className="item-img"
+                                />
+                            ) : (
+                                <IconShoppingBag className="item-img" />
+                            )}
+                            {!isDesktop && (
+                                <Stack
+                                    gap="sm"
+                                    style={{
+                                        marginLeft: "auto",
+                                        marginRight: "0px",
+                                    }}
+                                >
+                                    <ActionIcon
+                                        size="lg"
+                                        variant="subtle"
+                                        onClick={() => removeLinkedItem()}
+                                    >
+                                        <IconTrashFilled />
+                                    </ActionIcon>
+                                    <ActionIcon size="lg" variant="subtle">
+                                        <IconEdit />
+                                    </ActionIcon>
+                                </Stack>
+                            )}
+                            <Stack
+                                gap="sm"
+                                align="start"
+                                className="item-details"
+                            >
+                                <Group gap="sm">
+                                    <IconShoppingBag size={24} />
+                                    <Text>
+                                        {item.linked_item.name
+                                            .split(" ")
+                                            .map(capitalize)
+                                            .join(" ")}
+                                    </Text>
+                                </Group>
+                                <Group gap="sm">
+                                    <IconBuildingStore size={24} />
+                                    <Text>
+                                        {item.linked_item.type
+                                            .split(" ")
+                                            .map(capitalize)
+                                            .join(" ")}
+                                    </Text>
+                                </Group>
+                                {item.linked_item.location && (
+                                    <Group gap="sm">
+                                        <IconMapPin size={24} />
+                                        <Text>
+                                            {item.linked_item.location
+                                                .split(" ")
+                                                .map(capitalize)
+                                                .join(" ")}
+                                        </Text>
+                                    </Group>
+                                )}
+                                <Group gap="sm">
+                                    <IconCurrencyDollar size={24} />
+                                    <Text>
+                                        {item.linked_item.price
+                                            ? new Intl.NumberFormat("en-US", {
+                                                  style: "currency",
+                                                  currency: "USD",
+                                              })
+                                                  .format(
+                                                      item.linked_item.price
+                                                  )
+                                                  .slice(1)
+                                            : "0.00"}
+                                    </Text>
+                                </Group>
+                                <Group gap="sm" wrap="nowrap">
+                                    <IconCategoryFilled size={24} />
+                                    <Pill.Group>
+                                        <Group gap={4}>
+                                            {item.linked_item.categories.map(
+                                                (v, i) => (
+                                                    <Pill key={i}>{v}</Pill>
+                                                )
+                                            )}
+                                        </Group>
+                                    </Pill.Group>
+                                </Group>
+                                <Group gap="sm" wrap="nowrap">
+                                    <IconStarHalfFilled size={24} />
+                                    <Rating
+                                        value={item.linked_item.ratings.average}
+                                        fractions={2}
+                                        readOnly
+                                    />
+                                    ({item.linked_item.ratings.count})
+                                </Group>
+                            </Stack>
+                            {isDesktop && (
+                                <Stack gap="sm">
+                                    <ActionIcon
+                                        size="lg"
+                                        variant="subtle"
+                                        onClick={() => removeLinkedItem()}
+                                    >
+                                        <IconTrashFilled />
+                                    </ActionIcon>
+                                    <ActionIcon size="lg" variant="subtle">
+                                        <IconEdit />
+                                    </ActionIcon>
+                                </Stack>
+                            )}
+                        </Group>
+                    ) : (
+                        <Center p="lg">
+                            <Group gap="sm">
+                                <Button
+                                    size="lg"
+                                    leftSection={<IconLinkPlus size={24} />}
+                                >
+                                    {t("modals.editItem.actions.addLinked")}
+                                </Button>
+                            </Group>
+                        </Center>
+                    )}
+                </Paper>
+            </Stack>
+        </Modal>
+    );
+}
--- lia/src/views/ListViewer/ItemCard.tsx
+++ lia/src/views/ListViewer/ItemCard.tsx
@@ -5,7 +5,6 @@ import {
     Image,
     NumberInput,
     Paper,
-    Pill,
     Stack,
     Text,
 } from "@mantine/core";
@@ -22,6 +21,7 @@ import {
 import { useTranslation } from "react-i18next";
 import { isString } from "lodash";
 import { useApiMethods } from "../../api";
+import { ItemEditModal } from "../../modals/ItemEditModal/ItemEditModal";
 
 export function ListItemCard({
     list,
@@ -36,6 +36,7 @@ export function ListItemCard({
     const [checked, setChecked] = useState(item.checked);
     const [quantity, setQuantity] = useState(item.quantity.amount);
     const api = useApiMethods();
+    const [detailsOpen, setDetailsOpen] = useState<boolean>(false);
 
     useEffect(() => {
         if (quantity !== item.quantity.amount && api) {
@@ -46,103 +47,124 @@ export function ListItemCard({
     }, [quantity, item.quantity.amount]);
 
     return (
-        <Paper className="item-list-card" p="sm" radius="sm" shadow="sm">
-            <Stack gap="sm">
-                <Group justify="space-between" gap="sm">
-                    <Group align="center" gap="sm" wrap="nowrap">
-                        {item.linked_item && item.linked_item.images[0] ? (
-                            <Image
-                                w={48}
-                                h={48}
-                                src={item.linked_item.images[0]}
-                                radius={"sm"}
-                            />
-                        ) : (
-                            <IconShoppingBag size={48} />
-                        )}
-                        <Stack gap={2}>
-                            <Text fw="600">{item.name}</Text>
-                            <Group gap={2}>
-                                <IconCurrencyDollar size={16} />
-                                <Text>
-                                    {item.price
-                                        ? new Intl.NumberFormat("en-US", {
-                                              style: "currency",
-                                              currency: "USD",
-                                          }).format(item.price)
-                                        : "0.00"}
-                                </Text>
-                            </Group>
-                        </Stack>
-                    </Group>
-                    <Checkbox
-                        color="green"
-                        size="lg"
-                        checked={checked}
-                        onChange={(event) => {
-                            if (api) {
-                                api.list.setItemCheck(
-                                    access.type,
-                                    access.reference,
-                                    item.id,
-                                    event.target.checked
-                                );
-                                setChecked(event.target.checked);
-                            }
-                        }}
-                        onClick={(e) => e.stopPropagation()}
-                    />
-                </Group>
-                <Group gap="sm" justify="space-between" wrap="nowrap">
-                    {item.location ? (
-                        <Group gap={2}>
-                            <IconMapPin size={16} />
-                            <Text>{item.location}</Text>
+        <>
+            <Paper
+                className="item-list-card"
+                p="sm"
+                radius="sm"
+                shadow="sm"
+                onClick={() => setDetailsOpen(true)}
+            >
+                <Stack gap="sm">
+                    <Group justify="space-between" gap="sm">
+                        <Group align="center" gap="sm" wrap="nowrap">
+                            {item.linked_item && item.linked_item.images[0] ? (
+                                <Image
+                                    w={48}
+                                    h={48}
+                                    src={item.linked_item.images[0]}
+                                    radius={"sm"}
+                                />
+                            ) : (
+                                <IconShoppingBag size={48} />
+                            )}
+                            <Stack gap={2}>
+                                <Text fw="600">{item.name}</Text>
+                                <Group gap={2}>
+                                    <IconCurrencyDollar size={16} />
+                                    <Text>
+                                        {item.price
+                                            ? new Intl.NumberFormat("en-US", {
+                                                  style: "currency",
+                                                  currency: "USD",
+                                              })
+                                                  .format(item.price)
+                                                  .slice(1)
+                                            : "0.00"}
+                                    </Text>
+                                </Group>
+                            </Stack>
                         </Group>
-                    ) : (
-                        <Group gap={2}>
-                            <IconMapPin size={16} />
-                            <Text>{t("views.viewer.item.noLocation")}</Text>
-                        </Group>
-                    )}
-                    <NumberInput
-                        w={110}
-                        hideControls
-                        onClick={(e) => e.stopPropagation()}
-                        leftSection={
-                            <ActionIcon
-                                variant="default"
-                                className="qt-control decrease"
-                                onClick={() =>
-                                    setQuantity((c) => Math.max(0, c - 1))
+                        <Checkbox
+                            color="green"
+                            size="lg"
+                            checked={checked}
+                            onChange={(event) => {
+                                if (api) {
+                                    api.list.setItemCheck(
+                                        access.type,
+                                        access.reference,
+                                        item.id,
+                                        event.target.checked
+                                    );
+                                    setChecked(event.target.checked);
                                 }
-                            >
-                                <IconMinus size={16} />
-                            </ActionIcon>
-                        }
-                        rightSection={
-                            <ActionIcon
-                                variant="default"
-                                className="qt-control increase"
-                                onClick={() => setQuantity((c) => c + 1)}
-                            >
-                                <IconPlus size={16} />
-                            </ActionIcon>
-                        }
-                        suffix={
-                            item.quantity.unit
-                                ? " " + item.quantity.unit
-                                : undefined
-                        }
-                        value={quantity}
-                        onChange={(value) =>
-                            setQuantity(isString(value) ? 0 : value)
-                        }
-                        allowDecimal={false}
-                        className="item-quantity"
-                    />
-                </Group>
-            </Stack>
-        </Paper>
+                            }}
+                            onClick={(e) => e.stopPropagation()}
+                        />
+                    </Group>
+                    <Group gap="sm" justify="space-between" wrap="nowrap">
+                        {item.location ? (
+                            <Group gap={2}>
+                                <IconMapPin size={16} />
+                                <Text>{item.location}</Text>
+                            </Group>
+                        ) : (
+                            <Group gap={2}>
+                                <IconMapPin size={16} />
+                                <Text>{t("views.viewer.item.noLocation")}</Text>
+                            </Group>
+                        )}
+                        <NumberInput
+                            w={110}
+                            hideControls
+                            onClick={(e) => e.stopPropagation()}
+                            leftSection={
+                                <ActionIcon
+                                    variant="default"
+                                    className="qt-control decrease"
+                                    onClick={(e) => {
+                                        setQuantity((c) => Math.max(0, c - 1));
+                                        e.stopPropagation();
+                                    }}
+                                >
+                                    <IconMinus size={16} />
+                                </ActionIcon>
+                            }
+                            rightSection={
+                                <ActionIcon
+                                    variant="default"
+                                    className="qt-control increase"
+                                    onClick={(e) => {
+                                        setQuantity((c) => c + 1);
+                                        e.stopPropagation();
+                                    }}
+                                >
+                                    <IconPlus size={16} />
+                                </ActionIcon>
+                            }
+                            suffix={
+                                item.quantity.unit
+                                    ? " " + item.quantity.unit
+                                    : undefined
+                            }
+                            value={quantity}
+                            onChange={(value) =>
+                                setQuantity(isString(value) ? 0 : value)
+                            }
+                            allowDecimal={false}
+                            className="item-quantity"
+                        />
+                    </Group>
+                </Stack>
+            </Paper>
+            <ItemEditModal
+                item={item}
+                access={access}
+                list={list}
+                open={detailsOpen}
+                setOpen={setDetailsOpen}
+            />
+        </>
     );
 }
